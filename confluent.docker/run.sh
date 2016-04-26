b94bf2ee4dd33e1dc770ffe4b8cc7c0872680cc4
mengjingleli <mengjinglei@qiniu.com>
2016-04-26 16:16:14 +0800

    add two more docker file

--- confluent.docker/run.sh
+++ confluent.docker/run.sh
@@ -3,6 +3,9 @@ cd /app/confluent-2.0.1
 sleep 2
 ./bin/kafka-server-start ./etc/kafka/server.properties > kafka-server.out 2>&1 &
 sleep 2
+./bin/kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic tsdb-adapter
+./bin/kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic tsdb-adapter1
+./bin/kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic testAdditionTopic
 ./bin/schema-registry-start ./etc/schema-registry/schema-registry.properties > schema-registry.out 2>&1  &
 sleep 2
 ./bin/kafka-rest-start ./etc/kafka-rest/kafka-rest.properties 
